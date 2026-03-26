a2f5099201d9cce7918b6e5940bf8c9ed9b7d155
slobben <daantjesl@gmail.com>
2026-03-26 19:37:38 +0100

    update WebSocket endpoint to `/gen-api/ws`

--- src/main/java/slobben/Cells/config/WebSocketConfig.java
+++ src/main/java/slobben/Cells/config/WebSocketConfig.java
@@ -15,7 +15,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
+        registry.addEndpoint("/gen-api/ws").setAllowedOriginPatterns("*").withSockJS();
     }
 
 
