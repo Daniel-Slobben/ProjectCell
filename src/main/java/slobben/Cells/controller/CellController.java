ee9af8c4500f58833562c82460bc006adef41bc5
slobben <daantjesl@gmail.com>
2026-03-26 18:16:40 +0100

    update configuration files and add base request mapping to CellController

--- src/main/java/slobben/Cells/controller/CellController.java
+++ src/main/java/slobben/Cells/controller/CellController.java
@@ -6,16 +6,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 import slobben.Cells.config.StateInfo;
 import slobben.Cells.service.EnvironmentService;
 import slobben.Cells.service.RunnerService;
 
 @Controller
 @AllArgsConstructor
+@RequestMapping("gen-api")
 public class CellController {
 
     private static final Logger log = LoggerFactory.getLogger(CellController.class);
