5c335dc0de7f537bb691bb7a47a80b3f0f04c18d
slobben <daantjesl@gmail.com>
2026-04-13 09:18:06 +0200

    actually doing actual deltas now. atleast the debugger values dont look wrong ;)

--- src/main/java/slobben/cells/service/GenerationService.java
+++ src/main/java/slobben/cells/service/GenerationService.java
@@ -2,8 +2,6 @@ package slobben.cells.service;
 
 import slobben.cells.entities.model.Block;
 
-import java.util.LinkedList;
-
 public class GenerationService {
 
     @SuppressWarnings({"java:S3776", "java:S135"})
@@ -25,8 +23,8 @@ public class GenerationService {
             }
         }
 
-        LinkedList<Byte> deltas = new LinkedList<>();
-
+        int deltaIndex = -1;
+        byte[] deltas = new byte[blockSize * blockSize];
 
         for (int x = 1; x < blockSize + 1; x++) {
             for (int y = 1; y < blockSize + 1; y++) {
@@ -35,7 +33,9 @@ public class GenerationService {
                     if (heatmap[x][y] == 3) {
                         // now alive
                         block.getCells()[x][y] = true;
-                        setDeltaOperation((byte) 0b0_000000, deltas);
+                        deltaIndex = setDeltaOperation(true, deltas, deltaIndex);
+                    } else {
+                        deltaIndex = setDeltaOperation(false, deltas, deltaIndex);
                     }
                 }
                 // If cell was alive
@@ -43,17 +43,14 @@ public class GenerationService {
                     if (!(heatmap[x][y] == 2 || heatmap[x][y] == 3)) {
                         // now dead
                         block.getCells()[x][y] = false;
-                        setDeltaOperation((byte) 0b1_000000, deltas);
+                        deltaIndex = setDeltaOperation(true, deltas, deltaIndex);
+                    } else {
+                        deltaIndex = setDeltaOperation(false, deltas, deltaIndex);
                     }
                 }
             }
         }
-        byte[] byteDeltas = new byte[deltas.size()];
-        for (int i = 0; i < deltas.size(); i++) {
-            byteDeltas[i] = deltas.get(i);
-        }
-
-        block.addByteArrayToDelta(byteDeltas);
+        block.addByteArrayToDelta(deltas);
     }
 
     /**
@@ -62,30 +59,34 @@ public class GenerationService {
      * 1 -- changed
      * -
      * 7 least significant bits for how many in a row a cell has the same operation
-     * max = 128 times, starting at 0 (so 0_0000000 means the operation only applies to the current cell
-     *
-     * @param newOperation byte with only the two most significant bits (ex: (byte) 0b11_000000
-     * @param deltas       LinkedList with all previous bytes
+     * max = 128 times, starting at 1 (so 0_0000001 means the operation only applies to the current cell
      */
-    private static void setDeltaOperation(byte newOperation, LinkedList<Byte> deltas) {
-        int lastDeltaOperation = 0b0_100000;
-        if (!deltas.isEmpty()) {
-            lastDeltaOperation = deltas.peekLast() & 0x1_0000000;
-        }
-        if (lastDeltaOperation != newOperation || deltas.isEmpty()) {
-            deltas.addLast(newOperation);
-        } else {
-            byte delta = deltas.pollLast();
-            int operationAmount = delta & 0b0_1111111;
+    private static int setDeltaOperation(boolean cellChanged, byte[] deltas, int deltaIndex) {
+        byte lastDeltaOperation;
+        boolean noLastOperation = deltaIndex == -1;
+        boolean switchToOtherOperation = false;
 
-            if (operationAmount != 127) {
-                operationAmount++;
-                deltas.add((byte) (newOperation | operationAmount));
-            } else {
-                deltas.add(delta);
-                deltas.add((byte) lastDeltaOperation);
+        byte cellChangedOperation = cellChanged ? (byte) 0b1_000_0000 : (byte) 0b0_000_0000;
+
+        if (!noLastOperation) {
+            lastDeltaOperation = (byte) (deltas[deltaIndex] & 0b1_000_0000);
+            if (lastDeltaOperation != cellChangedOperation) {
+                switchToOtherOperation = true;
             }
         }
-    }
 
+        if (noLastOperation || switchToOtherOperation) {
+            deltas[++deltaIndex] = (byte) (cellChangedOperation | 0b0000_0001);
+        } else {
+            int operationAmount = deltas[deltaIndex] & 0b0_111_1111;
+
+            if (operationAmount != 0b111_1111) {
+                operationAmount++;
+                deltas[deltaIndex] = (byte) (cellChangedOperation | operationAmount);
+            } else {
+                deltas[++deltaIndex] = (byte) (cellChangedOperation | 0b0000_0001);
+            }
+        }
+        return deltaIndex;
+    }
 }
