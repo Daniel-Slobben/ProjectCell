4f9afa654b03f1f90d7efaf83638c64829018aee
slobben <daantjesl@gmail.com>
2026-07-10 13:17:26 +0200

    refactored linemaker. still work to do on chaos, but this is it for now.

--- src/main/java/slobben/cells/service/workers/chaos/makers/LineMaker.java
+++ src/main/java/slobben/cells/service/workers/chaos/makers/LineMaker.java
@@ -3,40 +3,44 @@ package slobben.cells.service.workers.chaos.makers;
 import slobben.cells.entities.Pattern;
 import slobben.cells.service.workers.chaos.ChaosHit;
 
-public class LineMaker implements Maker {
-    private static final int AMOUNT_OF_LINES = 6;
-    private static final int LINE_WIDTH = 2;
-    private static final int LINE_SIZE = 2000;
-    private static final int DISTANCE_BETWEEN_LINES = 200;
+import java.util.Random;
 
-    private static final boolean VERTICALS = true;
-    private static final boolean HORIZONTALS = true;
+public class LineMaker implements Maker {
+    private static final int MIN_AMOUNT_OF_LINES = 4;
+    private static final int MAX_AMOUNT_OF_LINES = 8;
+    private static final int LINE_WIDTH = 2;
+    private static final int SIZE = 2000;
+    private static final int MIN_DISTANCE_BETWEEN_LINES = 100;
+    private static final int MAX_DISTANCE_BETWEEN_LINES = 400;
+
+    private static final Random random = new Random();
 
     @Override
     public ChaosHit getChaosHit(int worldTargetX, int worldTargetY) {
-        boolean[][] matrix = new boolean[LINE_SIZE][LINE_SIZE];
+        int amountOfLines = random.nextInt(MIN_AMOUNT_OF_LINES, MAX_AMOUNT_OF_LINES + 1);
+        int distanceBetweenLines = random.nextInt(MIN_DISTANCE_BETWEEN_LINES, MAX_DISTANCE_BETWEEN_LINES + 1);
+        int size = SIZE;
+        int lineAmountLength = amountOfLines * distanceBetweenLines;
+        if (amountOfLines * distanceBetweenLines > size) {
+            size = lineAmountLength;
+        }
 
-        for (int i = 0; i < AMOUNT_OF_LINES; i++) {
-            int adjustedI = i * DISTANCE_BETWEEN_LINES;
-            for (int j = 0; j < LINE_SIZE; j++) {
+        boolean[][] matrix = new boolean[size][size];
+
+        for (int i = 0; i < amountOfLines; i++) {
+            int adjustedI = i * distanceBetweenLines;
+            for (int j = 0; j < size; j++) {
                 for (int line = 0; line < LINE_WIDTH; line++) {
-
-                    if (VERTICALS) {
-                        matrix[adjustedI + line][j] = true;
-                    }
-                    if (HORIZONTALS) {
-                        matrix[j][adjustedI + line] = true;
-                    }
+                    // verticals
+                    matrix[adjustedI + line][j] = true;
+                    // horizontals
+                    matrix[j][adjustedI + line] = true;
 
                 }
             }
         }
 
-        Pattern pattern = Pattern.builder()
-                .x(matrix.length)
-                .y(matrix[0].length)
-                .matrix(matrix)
-                .build();
-        return new ChaosHit(worldTargetX, worldTargetY, "LINES with size " + LINE_SIZE, pattern);
+        Pattern pattern = Pattern.builder().x(matrix.length).y(matrix[0].length).matrix(matrix).build();
+        return new ChaosHit(worldTargetX, worldTargetY, "LINES with size " + size, pattern);
     }
 }
