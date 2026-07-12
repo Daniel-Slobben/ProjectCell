a320fa9e0507e532260620f04839358928a5939b
slobben <daantjesl@gmail.com>
2026-07-12 16:28:49 +0200

    stable release for now. still need to fix reading resources from .jar

--- src/main/java/slobben/cells/service/workers/chaos/ChaosType.java
+++ src/main/java/slobben/cells/service/workers/chaos/ChaosType.java
@@ -1,13 +1,16 @@
 package slobben.cells.service.workers.chaos;
 
-import slobben.cells.service.workers.chaos.makers.*;
+import slobben.cells.service.workers.chaos.makers.LineMaker;
+import slobben.cells.service.workers.chaos.makers.Maker;
+import slobben.cells.service.workers.chaos.makers.SquareInSquareMaker;
+import slobben.cells.service.workers.chaos.makers.SquareMaker;
 
 enum ChaosType {
     SQUARE(new SquareMaker()),
     SQUARE_IN_SQUARE(new SquareInSquareMaker()),
-    LINE_MAKER(new LineMaker()),
-    GROWTH_PATTERN(new GrowthMaker()),
-    OSCILLATORS(new OscillatorMaker());
+    LINE_MAKER(new LineMaker());
+//    GROWTH_PATTERN(new GrowthMaker()),
+//    OSCILLATORS(new OscillatorMaker());
 
     final Maker maker;
 
