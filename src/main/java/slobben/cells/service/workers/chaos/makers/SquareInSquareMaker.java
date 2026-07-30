2483b6ae437fc54efbfa801df6f96d8989e031e1
slobben <daantjesl@gmail.com>
2026-07-30 16:22:19 +0200

    new square in square experiment

--- src/main/java/slobben/cells/service/workers/chaos/makers/SquareInSquareMaker.java
+++ src/main/java/slobben/cells/service/workers/chaos/makers/SquareInSquareMaker.java
@@ -3,40 +3,50 @@ package slobben.cells.service.workers.chaos.makers;
 import slobben.cells.entities.Pattern;
 import slobben.cells.service.workers.chaos.ChaosHit;
 
+import java.util.Random;
+
 public class SquareInSquareMaker implements Maker {
-    private static final int SQUARE_SIZE = 2500;
-    private static final int DISTANCE_BETWEEN_SQUARES = 300;
-    private static final int AMOUNT_OF_SQUARES = 7;
+    private static final int SQUARE_SIZE_MIN = 2000;
+    private static final int SQUARE_SIZE_MAX = 4000;
+    private static final int STARTING_DISTANCE_MIN = 25;
+    private static final int STARTING_DISTANCE_MAX = 100;
+    private static final double GROWTH_MULTIPLIER = 1.0;
+
+    private final Random random = new Random();
 
     @Override
     public ChaosHit getChaosHit(int worldTargetX, int worldTargetY) {
-        boolean[][] matrix = new boolean[SQUARE_SIZE][SQUARE_SIZE];
+        final int maxSquareSize = random.nextInt(SQUARE_SIZE_MIN, SQUARE_SIZE_MAX);
+        boolean[][] matrix = new boolean[maxSquareSize][maxSquareSize];
 
-        for (int k = AMOUNT_OF_SQUARES; k >= 0; k--) {
-            int squareSize = SQUARE_SIZE - (k * DISTANCE_BETWEEN_SQUARES);
-            int distanceFromBorder = Math.abs(squareSize - SQUARE_SIZE) / 2;
+        int squareSize = random.nextInt(STARTING_DISTANCE_MIN, STARTING_DISTANCE_MAX);
+
+        while (true) {
+            int distanceFromBorder = (maxSquareSize / 2) - (squareSize / 2);
+            if (distanceFromBorder < 0) {
+                break;
+            }
             for (int i = 0; i < squareSize; i++) {
                 int adjustedI = i + distanceFromBorder;
-
                 matrix[distanceFromBorder][adjustedI] = true;
                 matrix[1 + distanceFromBorder][adjustedI] = true;
 
-                matrix[SQUARE_SIZE - 1 - distanceFromBorder][adjustedI] = true;
-                matrix[SQUARE_SIZE - 2 - distanceFromBorder][adjustedI] = true;
+                matrix[maxSquareSize - 1 - distanceFromBorder][adjustedI] = true;
+                matrix[maxSquareSize - 2 - distanceFromBorder][adjustedI] = true;
 
                 matrix[adjustedI][distanceFromBorder] = true;
                 matrix[adjustedI][1 + distanceFromBorder] = true;
 
-                matrix[adjustedI][SQUARE_SIZE - 1 - distanceFromBorder] = true;
-                matrix[adjustedI][SQUARE_SIZE - 2 - distanceFromBorder] = true;
+                matrix[adjustedI][maxSquareSize - 1 - distanceFromBorder] = true;
+                matrix[adjustedI][maxSquareSize - 2 - distanceFromBorder] = true;
             }
-
+            squareSize = squareSize + (int) (squareSize * GROWTH_MULTIPLIER);
         }
         Pattern pattern = Pattern.builder()
                 .x(matrix.length)
                 .y(matrix[0].length)
                 .matrix(matrix)
                 .build();
-        return new ChaosHit(worldTargetX, worldTargetY, "Square with size " + SQUARE_SIZE, pattern);
+        return new ChaosHit(worldTargetX + maxSquareSize / 2, worldTargetY + maxSquareSize / 2, "Square with size " + maxSquareSize, pattern);
     }
 }
