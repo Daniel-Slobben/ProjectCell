2829cb6f64610841eaf519dc94d08270f0586cd3
slobben <daantjesl@gmail.com>
2026-08-03 13:31:38 +0200

    lettucemaker bugfix

--- src/main/java/slobben/cells/service/workers/chaos/makers/LettuceMaker.java
+++ src/main/java/slobben/cells/service/workers/chaos/makers/LettuceMaker.java
@@ -19,7 +19,8 @@ public class LettuceMaker implements Maker {
         int amountOfLines = random.nextInt(MIN_AMOUNT_OF_LINES, MAX_AMOUNT_OF_LINES + 1);
         int size = random.nextInt(MIN_SIZE, MAX_SIZE + 1);
         int distanceBetweenLines = size / amountOfLines;
-        size -= distanceBetweenLines + 2;
+        size -= distanceBetweenLines;
+        size += 2;
 
         boolean[][] matrix = new boolean[size][size];
 
